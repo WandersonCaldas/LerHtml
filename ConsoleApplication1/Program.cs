90e3a90efff5b88446e92acccaa2d7a7e33a1f2f
Wanderson Caldas <wcaldasti@gmail.com>
2020-09-16 16:04:16 -0300

    CRIAR PROJETO

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strSiteUrl = "URL";
+
+            var request = (HttpWebRequest)WebRequest.Create(strSiteUrl);
+            var response = (HttpWebResponse)request.GetResponse();
+            var stream = response.GetResponseStream();
+            var streamReader = new StreamReader(stream);
+            //Console.WriteLine(streamReader.ReadToEnd());
+
+            string[] lines = { streamReader.ReadToEnd() };
+            System.IO.File.WriteAllLines(@"C:\Downloads\WriteLines.txt", lines);
+        }
+    }
+}
